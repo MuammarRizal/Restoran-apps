4091807e51a75724fe07eb7d2ccd1433174c0745
MuammarRizal <muammarrizal31@gmail.com>
2024-11-04 10:21:17 +0700

    push production

--- src/utils/localenv.js
+++ src/utils/localenv.js
@@ -1,4 +1,4 @@
-export const ApiLocal = "http://localhost:5000/api";
+// export const ApiLocal = "http://localhost:5000/api";
 
 // sesuaikan sama IP address
-// export const ApiLocal = "http://192.168.100.131:5000/api";
+export const ApiLocal = "http://192.168.88.191:5000/api";
